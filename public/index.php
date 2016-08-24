6d39dc51b7dd253228a5b4abc03b7949608e59d8
bahkali <kalimamadou@gmail.com>
2016-08-24 01:08:33 -0700

    working on comment class

--- public/index.php
+++ public/index.php
@@ -1,12 +1,19 @@
-<?php
-require_once("../includes/initialize.php");
+<?php require_once("../includes/initialize.php");?>
+<?php 
+//find all photos
+  $photos = Photograph::find_all();  
 
-
-include_layout_template("header.php"); ?>
-
-
-<h2>Welcome To Photo Gallery</h2>
-
-<?php
-include_layout_template("footer.php");
 ?>
+<?php include_layout_template("header.php"); ?>
+
+
+<?php foreach($photos as $photo):?>
+    <div style="float:left; margin-left:20px;">
+       <a href="photo.php?id=<?php echo $photo->id;?>">
+        <img src="<?php echo $photo->image_path() ; ?>" width="200" height="200">
+        </a>
+        <p><?php echo $photo->caption;?></p>
+    </div>
+<?php endforeach; ?>
+
+<?php include_layout_template("footer.php"); ?>
